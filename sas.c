34c1ae7e86bbe44a96ef6c995c8e0666cb154cce
amineElgaini <elgainiamine@gmail.com>
2025-09-10 12:27:38 +0100

    sas

--- sas.c
+++ sas.c
@@ -9,6 +9,9 @@ enum Funcs
     ADD = 1,
     DELETE,
     SEARCH,
+    SEARCH_PLAYERS, //
+    ORDER,
+
     DISPLAY,
     UPDATE,
     SORT,
@@ -16,13 +19,26 @@ enum Funcs
     EXIT
 };
 
+enum SEARCH{
+    SEARCH_ID=1,
+    SEARCH_NOM,
+    
+    SEARCH_POSTS, // each post
+    SEARCH_ORDER_NOM, // nom alphabetique 
+    SEARCH_ORDER_AGE, // order age
+
+    SEARCH_POST, // one post
+    SEARCH_TRANCH_AGE, // age [ ]
+};
+
 typedef struct DateInscription {
-    char year[100];
-    char month[100];
-    char day[100];
+    int year;
+    int month;
+    int day;
 } DateInscription;
 
-char validPosts[][100] = {"fardien", "defenseir", "milieu", "attaquant"};
+int requireButsToBeAstar = 10;
+char validPosts[][100] = {"gardien", "defenseir", "milieu", "attaquant"};
 char validStatus[][100] = {"titulaire", "remplacant"};
 
 typedef struct Player {
@@ -65,7 +81,7 @@ void saveToFile(Player *arr) {
 }
 
 void loadFromFile(Player **arr) {
-    FILE *fp = fopen("./player.dat", "rb");
+    FILE *fp = fopen("./players.dat", "rb");
     if (fp == NULL) {
         printf("No existing data file found. Starting fresh.\n");
         player_count = 0;
@@ -105,6 +121,14 @@ void loadFromFile(Player **arr) {
     printf("Loaded %d player from file.\n", player_count);
 }
 
+void printPlayer(Player arr) {
+    printf("Id: %d, Nom: %s, Prenom: %s, numeroMaillot: %d, post: %s, age:%d, buts: %d, date: %d-%02d-%02d, status: %s\n", 
+               (arr).id, (arr).nom, (arr).prenom, 
+               (arr).numerMaillot, (arr).post, 
+               (arr).age, (arr).buts, (arr).dateInscription.year,
+               (arr).dateInscription.month, (arr).dateInscription.day, (arr).status);
+}
+
 void addPlayer(Player **arr) {
     char add = 'y';
 
@@ -123,13 +147,13 @@ void addPlayer(Player **arr) {
         scanf("%d", &newPlayer.numerMaillot);
         clear_buffer();
         
-        bool isNotValidPost = false;
+        bool isNotValidPost = true;
         do {
             printf("Enter player post: ");
-            scanf("%d", &newPlayer.post);
+            scanf("%s", &newPlayer.post);
             clear_buffer();
             for(int i=0;i<sizeof(validPosts)/sizeof(validPosts[0]);i++) {
-                if (validPosts[i]==newPlayer.post) {
+                if (strcmp(validPosts[i], newPlayer.post)==0) {
                     isNotValidPost = false;
                     break;
                 }
@@ -152,13 +176,17 @@ void addPlayer(Player **arr) {
         int month = currentTime->tm_mon + 1;
         int day   = currentTime->tm_mday;
 
-        bool isNotValidStatus = false;
+        newPlayer.dateInscription.year = year;
+        newPlayer.dateInscription.month = month;
+        newPlayer.dateInscription.day = day;
+
+        bool isNotValidStatus = true;
         do {
             printf("Enter player status: ");
-            scanf("%d", &newPlayer.status);
+            scanf("%s", &newPlayer.status);
             clear_buffer();
             for(int i=0;i<sizeof(validStatus)/sizeof(validStatus[0]);i++) {
-                if (validStatus[i]==newPlayer.status) {
+                if (strcmp(validStatus[i], newPlayer.status)==0) {
                     isNotValidStatus = false;
                     break;
                 }
@@ -176,10 +204,13 @@ void addPlayer(Player **arr) {
 
         saveToFile(*arr);
         printf("Player added successfully!\n");
+        if (newPlayer.buts >= requireButsToBeAstar) {
+            printf("star de l'equipe\n");
+        }
 
         printf("If you wanna add again type 'y': ");
         scanf("%c", &add);
-
+        clear_buffer();
     }
 }
 
@@ -229,11 +260,7 @@ void displayPlayers(Player **arr) {
     }
 
     for (int i = 0; i < player_count; i++) {
-        printf("Id: %d, Nom: %s, Prenom: %s, numeroMaillot: %d, post: %s, age:%s, buts: %d, date: %d-%02d-%02d, status: %s\n", 
-               (*arr)[i].id, (*arr)[i].nom, (*arr)[i].prenom, 
-               (*arr)[i].numerMaillot, (*arr)[i].post, 
-               (*arr)[i].age, (*arr)[i].buts, (*arr)[i].dateInscription.year,
-               (*arr)[i].dateInscription.month, (*arr)[i].dateInscription.day, (*arr)[i].status);
+        printPlayer((*arr)[i]);
     }
 }
 
@@ -259,28 +286,59 @@ void sortPlayer(Player **arr) {
     displayPlayers(arr);
 }
 
+
+enum SEARCH_PLAYER {
+    ID=1,
+    NOM
+};
 void searchPlayer(Player **arr) {
     if (player_count == 0) {
         printf("No players to search!\n");
         return;
     }
-
-    char nom[100];
-    printf("Enter player nom: ");
-    fgets(nom, sizeof(nom), stdin);
-    clear_newline(nom);
-
+    int choix;
+    printf("Enter CHOIX [1] id, [2] nom: ");
+    scanf("%d", &choix);
+    clear_buffer();
+    
     int found = 0;
-    for (int i = 0; i < player_count; i++) {
-        if (strcmp((*arr)[i].nom, nom) == 0) {
-            printf("Id: %d, Nom: %s, Prenom: %s, numeroMaillot: %d, post: %s, age:%s, buts: %d, date: %d-%02d-%02d, status: %s\n", 
-               (*arr)[i].id, (*arr)[i].nom, (*arr)[i].prenom, 
-               (*arr)[i].numerMaillot, (*arr)[i].post, 
-               (*arr)[i].age, (*arr)[i].buts, (*arr)[i].dateInscription.year,
-               (*arr)[i].dateInscription.month, (*arr)[i].dateInscription.day, (*arr)[i].status);
-            found = 1;
-            break;
+    switch (choix)
+    {
+    case ID: {
+
+        int id;
+        printf("Enter player id: ");
+        scanf("%d", &id);
+        clear_buffer();
+
+        for (int i = 0; i < player_count; i++) {
+            if ((*arr)[i].id == id) {
+                printPlayer((*arr)[i]);
+                found = 1;
+                break;
+            }
         }
+        break;
+    } case NOM: {
+
+        char nom[100];
+        printf("Enter player nom: ");
+        fgets(nom, sizeof(nom), stdin);
+        clear_newline(nom);
+
+        for (int i = 0; i < player_count; i++) {
+            if (strcmp((*arr)[i].nom, nom) == 0) {
+                printPlayer((*arr)[i]);
+                found = 1;
+                break;
+            }
+        }
+        break;
+    }
+        default:
+
+            printf("Out of available options!\n");
+            break;
     }
     
     if (!found) {
@@ -294,20 +352,40 @@ void updatePlayer(Player **arr) {
         return;
     }
 
-    char nom[100];
-    int age;
-    printf("Enter player nom: ");
-    fgets(nom, sizeof(nom), stdin);
-    clear_newline(nom);
+    int id;
+    printf("Enter player id that you wanna update: ");
+    scanf("%d", &id);
+    clear_buffer();
 
-    printf("Enter new age: ");
+    char post[100];
+    int age, buts;
+    bool isNotValidPost = true;
+    do {
+        printf("Enter player post: ");
+        scanf("%s", &post);
+        clear_buffer();
+        for(int i=0;i<sizeof(validPosts)/sizeof(validPosts[0]);i++) {
+            if (strcmp(validPosts[i], post)==0) {
+                isNotValidPost = false;
+                break;
+            }
+        }
+    } while(isNotValidPost);
+
+    printf("Enter player age: ");
     scanf("%d", &age);
+    clear_buffer();
+
+    printf("Enter player buts: ");
+    scanf("%d", &buts);
     clear_buffer();
 
     int found = 0;
     for (int i = 0; i < player_count; i++) {
-        if (strcmp((*arr)[i].nom, nom) == 0) {
+        if ((*arr)[i].id == id) {
             (*arr)[i].age = age;
+            strcpy((*arr)[i].post, post);
+            (*arr)[i].buts = buts;
             found = 1;
             break;
         }
@@ -321,6 +399,139 @@ void updatePlayer(Player **arr) {
     }
 }
 
+enum SEARCH_PLAYERS {
+    POST=1,
+    POSTS,
+    AGE_TRANCHE
+};
+
+void searchPlayers(Player **arr) {
+
+    if (player_count == 0) {
+        printf("No players to search!\n");
+        return;
+    }
+    int choix;
+    printf("Enter CHOIX [1] post, [2] posts ,[3] age tranche:");
+    scanf("%d", &choix);
+    clear_buffer();
+    
+    int found = 0;
+    switch (choix)
+    {
+    case POST: {
+
+        char post[100];
+        printf("Enter post: ");
+        fgets(post, sizeof(post), stdin);
+        clear_newline(post);
+
+        for (int i = 0; i < player_count; i++) {
+            if (strcmp((*arr)[i].post, post) == 0) {
+                printPlayer((*arr)[i]);
+                found = 1;
+            }
+        }
+        break;
+    } case POSTS: {
+
+        int postCount = sizeof(validPosts) / sizeof(validPosts[0]);
+        for (int i = 0; i < postCount; i++) {
+            printf("Post: %s\n", validPosts[i]);
+            for (int j = 0; j < player_count; j++) {
+                if (strcmp((*arr)[j].post, validPosts[i]) == 0) {
+                    printPlayer((*arr)[j]);
+                    found = 1;
+                    break;
+                }
+            }
+        }
+
+        break;
+    }
+     case AGE_TRANCHE: {
+
+        int minAge;
+        printf("Enter player min age: ");
+        scanf("%d", &minAge);
+        clear_buffer();
+
+        int maxAge;
+        printf("Enter player max age: ");
+        scanf("%d", &maxAge);
+        clear_buffer();
+
+        for (int i = 0; i < player_count; i++) {
+            if ((*arr)[i].age >= minAge && (*arr)[i].age <= maxAge) {
+                printPlayer((*arr)[i]);
+                found = 1;
+                break;
+            }
+        }
+        break;
+    }
+        default:
+
+            printf("Out of available options!\n");
+            break;
+    }
+    
+    if (!found) {
+        printf("Nothing Found!\n");
+    }
+}
+
+enum SEARCH_PLAYER {
+    SORT_AGE=1,
+    SORT_NOM
+};
+void order(Player **arr) {
+    if (player_count == 0) {
+        printf("No players to sort!\n");
+        return;
+    }
+
+    Player temp;
+
+        int choix;
+    printf("Enter CHOIX [1] post, [2] posts ,[3] age tranche:");
+    scanf("%d", &choix);
+    clear_buffer();
+    
+    int found = 0;
+    switch (choix)
+    {
+        case SORT_AGE: {
+            for (int i = 0; i < player_count - 1; i++) {   
+                for (int j = 0; j < player_count - i - 1; j++) {   
+                    if ((*arr)[j].age > (*arr)[j+1].age) {
+                        temp = (*arr)[j];
+                        (*arr)[j] = (*arr)[j + 1];
+                        (*arr)[j + 1] = temp;
+                    }
+                }
+            }
+        } case SORT_NOM: {
+            for (int i = 0; i < player_count - 1; i++) {   
+                for (int j = 0; j < player_count - i - 1; j++) {   
+                    if ((*arr)[j].age > (*arr)[j+1].age) {
+                        temp = (*arr)[j];
+                        (*arr)[j] = (*arr)[j + 1];
+                        (*arr)[j + 1] = temp;
+                    }
+                }
+            }
+        } default:
+
+        break;
+    }
+
+    
+    saveToFile(*arr);
+    printf("Players sorted successfully!\n");
+    displayPlayers(arr);
+}
+
 int main() {
     Player *Players = NULL;
     loadFromFile(&Players);
@@ -328,12 +539,16 @@ int main() {
         printf("\n=== Player Management System ===\n");
         printf("Enter 1 to add a player\n");
         printf("Enter 2 to delete a player\n");
-        printf("Enter 3 to search a player\n");
-        printf("Enter 4 to display all players\n");
-        printf("Enter 5 to update player quantity\n");
-        printf("Enter 6 to sort player by year\n");
-        printf("Enter 7 to calculate total inventory value\n");
-        printf("Enter 8 to exit\n");
+
+        printf("Enter 3 to search a player\n"); // id prenom
+        printf("Enter 4 to search players\n"); // post, postes, age tranch
+        printf("Enter 5 to order\n"); // tri age, tri prenom
+
+        printf("Enter 6 to display all players\n");
+        printf("Enter 7 to update player quantity\n");
+        printf("Enter 8 to sort player by year\n");
+        printf("Enter 9 to calculate total inventory value\n");
+        printf("Enter 10 to exit\n");
         printf("Your choice: ");
         
         int choice;
@@ -354,6 +569,14 @@ int main() {
         case SEARCH:
             searchPlayer(&Players);
             break;
+        case SEARCH_PLAYERS:
+            searchPlayers(&Players);
+            break;
+        case ORDER:
+            order(&Players);
+            break;
+
+
         case DISPLAY:
             displayPlayers(&Players);
             break;
